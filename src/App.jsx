9424b631a6061955675c7b9c025ffbd90d8721c0
cristobendito <flopybbb@gmail.com>
2024-06-04 16:05:37 +0200

    player ok arreglar banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import { useState } from 'react'
-import './App.css'
+import Navbar from './components/navbar/navbar';
+/* import './App.css' */
 import MediaControlCard from './components/play/player';
 
 
 function App() {
-  const [count, setCount] = useState(0)
+/*   const [count, setCount] = useState(0) */
 
 
   return (
     <div className="App">
-     <MediaControlCard />
-{/*       <Navbar />
+    <MediaControlCard />
+    <Navbar />
 
-      <Footer /> */}
     </div>
   );
 }
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,2 +1,37 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './navbar.css';
+import logoGif from '../../assets/sexpeares3D.gif';
+
+const Navbar = () => {
+   const handleNavigation = (page) => {
+    // logica para navegar pagina correspondiente
+    console.log(`Navegando a ${page}`);
+  }; 
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <img src={logoGif} alt="Logo" className="logo-img" />
+      </div>
+      <div className="navbar-links">
+        <button className="navbar-button" onClick={() => handleNavigation('Home')}>
+          Home
+        </button>
+        <button className="navbar-button" onClick={() => handleNavigation('Discos')}>
+          Discos
+        </button>
+        <button className="navbar-button" onClick={() => handleNavigation('Conciertos')}>
+          Conciertos
+        </button>
+        <button className="navbar-button" onClick={() => handleNavigation('Videos')}>
+          Videos
+        </button>
+        <button className="navbar-button" onClick={() => handleNavigation('Tienda')}>
+          Tienda
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
